c98253ff36291415e78d04fa3ccfd3bccf157e69
viniman <vinicius.carlos@live.com>
2020-09-03 14:52:50 -0300

    Create CRUD

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -10,9 +10,29 @@ module.exports = {
         return res.json(products);
     },
 
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
     async store(req, res) {
         const product = await Product.create(req.body);
 
         return res.json(product);
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true})
+        // new: true = vai retornar o produto depois de atualizado
+        // se nao passar esse parametro, ele vai retornar antes de atualizar
+
+        return res.json(product);
+    },
+
+    async destroy(req, res) {
+        await Product.findByIdAndRemove(req.params.id)
+
+        return res.send(); // retornar resposta de sucesso sem conteudo
     }
 };
