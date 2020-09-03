5265c6ed43dd71c9bcd8eeb05bb5f03046876eb0
viniman <vinicius.carlos@live.com>
2020-09-03 20:29:52 -0300

    List products from huntweb and makes add styles to page list product

--- huntweb-react/src/pages/main/index.js
+++ huntweb-react/src/pages/main/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import './styles.css';
+
 export default class Main extends Component {
+    // variavel de estado
+    state = {
+        products: []
+    }
+
+
     // metodo que é executado assim que o componente for mostrado em tela
     componentDidMount() {
         this.loadProducts();
@@ -10,10 +18,26 @@ export default class Main extends Component {
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        //console.log(response.data.docs);
+
+        this.setState({ products: response.data.docs })
     };
     
     render() {
-        return <h1>Hello Rocketseat</h1>;
+        const { products } = this.state; //desestruturacao
+    return (
+        <div className="product-list">
+            {  products.map(product =>(
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>
+                </article>
+            ))}
+        </div>
+    )
+    
+        //return <h1>Contagem de produtos: { this.state.products.length }</h1>;
     };
 };
