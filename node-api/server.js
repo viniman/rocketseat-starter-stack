a1a663bcd5d44a824a06fdea88c12e6c5704cd99
viniman <vinicius.carlos@live.com>
2020-09-03 06:50:41 -0300

    Use mongodb and mongoose for db entries

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+// Require dos models
+requireDir('./src/models');
+
+
+const Product = mongoose.model('Product');
+
+// Primeira rota
 app.get("/", (req, res) => {
     res.send("Hello Viniman!");
+
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
 });
 
 app.listen(3001);
