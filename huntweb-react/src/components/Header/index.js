bbbfa618916d09a92c0263b7695ac675e4d69ebb
viniman <vinicius.carlos@live.com>
2020-09-03 22:19:27 -0300

    Detail and styling the navigation and product page of huntweb project

--- huntweb-react/src/components/Header/index.js
+++ huntweb-react/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './styles.css';
 
-const Header = () => (<header id="main-header">JSHunt</header>);
+const Header = () => (<header id="main-header"><a href='/'>JSHunt</a></header>);
 
 export default Header;
 
